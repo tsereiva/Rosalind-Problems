4c54362d3642c74a64a19ff67dd403abd1d92255
tsereiva <zvejys7@gmail.com>
2020-09-11 12:39:24 -0400

    Fixes output format, but overlapping motifs are not being found

--- motif_dna.py
+++ motif_dna.py
@@ -1,8 +1,13 @@
 import re
-with open("rosalind_subs.txt", "r") as my_file:
+with open("test.txt", "r") as my_file:
     s,t = my_file.readlines()
     sequence = s.rstrip()
     motif = t.rstrip()
     Q=re.compile(motif)
-    output = [item.start(0) for item in Q.finditer(sequence)]
-    print(output)
+    output = [item.start(0)+1 for item in Q.finditer(sequence)]
+#https://stackoverflow.com/questions/3590165/join-a-list-of-items-with-different-types-as-string-in-python
+#Explains iterative .join function
+    print(' '. join(str(x) for x in output))
+
+#https://stackoverflow.com/questions/22747427/looking-for-the-motifs-in-sequence
+#Helpful for understanding
