ee0b383e7002d040ee8a5cc479719de3f251970b
tsereiva <zvejys7@gmail.com>
2020-09-11 14:31:27 -0400

    Replaces buit-in regex module with 3rd party regex module which supports overlapping motifs

--- motif_dna.py
+++ motif_dna.py
@@ -1,13 +1,13 @@
-import re
-with open("test.txt", "r") as my_file:
+import regex as re
+with open("rosalind_subs.txt", "r") as my_file:
     s,t = my_file.readlines()
     sequence = s.rstrip()
     motif = t.rstrip()
     Q=re.compile(motif)
-    output = [item.start(0)+1 for item in Q.finditer(sequence)]
+    output = [item.start(0)+1 for item in Q.finditer(sequence, overlapped = True)]
 #https://stackoverflow.com/questions/3590165/join-a-list-of-items-with-different-types-as-string-in-python
 #Explains iterative .join function
     print(' '. join(str(x) for x in output))
 
-#https://stackoverflow.com/questions/22747427/looking-for-the-motifs-in-sequence
-#Helpful for understanding
+#https://stackoverflow.com/questions/5616822/python-regex-find-all-overlapping-matches
+#Helped find third-party regex module that supports overlapping matches
