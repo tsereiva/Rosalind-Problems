0ebd160f13ec933e1f9282af7b150cd3afda020b
tsereiva <zvejys7@gmail.com>
2020-09-14 00:41:34 -0400

    Redone to calculate protein mass using monoisotopic values

--- protein_mass.py
+++ protein_mass.py
@@ -1,6 +1,28 @@
-from Bio.SeqUtils.ProtParam import ProteinAnalysis
-#https://biopython.org/wiki/ProtParam
+monoiso = {
+'A': 71.03711,
+'C': 103.00919,
+'D': 115.02694,
+'E': 129.04259,
+'F': 147.06841,
+'G': 57.02146,
+'H': 137.05891,
+'I': 113.08406,
+'K': 128.09496,
+'L': 113.08406,
+'M': 131.04049,
+'N': 114.04293,
+'P': 97.05276,
+'Q': 128.05858,
+'R': 156.10111,
+'S': 87.03203,
+'T': 101.04768,
+'V': 99.06841,
+'W': 186.07931,
+'Y': 163.06333
+}
 with open('rosalind_prtm.txt') as my_file:
     protein = my_file.read().strip()
-    analysed_seq = ProteinAnalysis(protein)
-    print(analysed_seq.molecular_weight())
+    mass = 0
+    for item in protein:
+        mass += monoiso[item]
+print(mass)
