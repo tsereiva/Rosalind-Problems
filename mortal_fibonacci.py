fa9772ac3471da57b5192135f7bd475efb22d433
tsereiva <zvejys7@gmail.com>
2020-09-17 23:32:51 -0400

    Non-working

--- mortal_fibonacci.py
+++ mortal_fibonacci.py
@@ -1,8 +1,9 @@
 def mortal_fibonacci(n,m):
-    if n < 2:
+    if n > 2:
         return(n)
-    while n < m:
-        return(mortal_fibonacci(n-1, m) + (mortal_fibonacci(n-2, m)))
-    while n > m:
-        return(mortal_fibonacci(n-1, m) + (mortal_fibonacci(n-2, m)) - mortal_fibonacci(n-(int(m)+1), m))
+    elif 2 <= n <= m:
+        return(mortal_fibonacci(n-1,m) + mortal_fibonacci(n-2,m))
+    elif n > m:
+        return(mortal_fibonacci(n-1,m) + mortal_fibonacci(n-2,m)-mortal_fibonacci(n-3,m))
+
 print(mortal_fibonacci(6,3))
